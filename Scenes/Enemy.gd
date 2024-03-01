71fe046c88fc1e0a1cf304ecc3630dcf41b35aff
DiegzM <147036619+DiegzM@users.noreply.github.com>
2024-02-29 22:58:40 -0800

    Pre-Alpha 1.3
    Added dashing mechanism, temporary enemy ai

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -1,44 +1,43 @@
 extends CharacterBody2D
 
-@export var WALKSPEED = 65
-@export var SPRINTSPEED = 265
+@export var WALK_SPEED = 265
 @export var ACCELERATION = 1000
 @export var FRICTION = 1000
 @export var STAIRS_MULTIPLIER = 0.75
-@export var DASH_MULTIPLIER = 1.5
-@export var DASH_FRICTION = 400
 
 enum StairTypes {NONE, UP_DOWN, LEFT, RIGHT}
 
+@onready var player = get_parent().get_node("Player")
 @onready var axis = Vector2.ZERO
 @onready var target_speed = 0
+@onready var sword_rotation = 0
+@onready var swing = 0
+@onready var swinging = false
 @onready var stair_type = StairTypes.NONE
 @onready var y_bias = 0
 @onready var speed_multiplier = 1
-@onready var left_click_pressed = false
-@onready var left_click_just_pressed = false
-@onready var slashing = false
 
-func _unhandled_input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				left_click_pressed = true
-				left_click_just_pressed = true
-			else:
-				left_click_pressed = false
-				left_click_just_pressed = false
-				
-func update_just_pressed():
-	if left_click_just_pressed:
-		left_click_just_pressed = false
-	
 func _physics_process(delta):
-	if left_click_just_pressed or slashing:
-		slash(delta)
+	move(delta)
+	swing_sword(delta)
+	update_sword(delta)
+
+func swing_sword(delta):
+	var direction_axis = (player.get_position() - self.global_position)
+	if direction_axis.length() < 130:
+		if not swinging:
+			swing = -120
+			swinging = true
+		else:
+			swing += RandomNumberGenerator.new().randi_range(14,25)
+			if swing > 120:
+				swinging = false
 	else:
-		move(delta)
-	update_just_pressed()
+		swing = 0
+	
+func update_sword(delta):
+	sword_rotation = atan2(axis.y, axis.x) + deg_to_rad(45 + swing)
+	$Sword.rotation = sword_rotation
 	
 func move(delta):
 	match stair_type:
@@ -53,9 +52,7 @@ func move(delta):
 		StairTypes.NONE:
 			y_bias = 0
 			speed_multiplier = 1
-			
-	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	axis.y = y_bias + int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	axis = player.get_position() - self.global_position
 		
 	axis = axis.normalized()
 	
@@ -65,10 +62,7 @@ func move(delta):
 		else:
 			velocity = Vector2.ZERO
 	else:
-		if (Input.is_key_pressed(KEY_SHIFT)):
-			target_speed = SPRINTSPEED * speed_multiplier
-		else:
-			target_speed = WALKSPEED * speed_multiplier
+		target_speed = WALK_SPEED * speed_multiplier
 		velocity += (axis * ACCELERATION * delta)
 		velocity = velocity.limit_length(target_speed)
 	
@@ -76,24 +70,12 @@ func move(delta):
 		$Sprite2D.scale.x = -abs($Sprite2D.scale.x)
 	elif velocity.x > 0:
 		$Sprite2D.scale.x = abs($Sprite2D.scale.x)
-		
+	
 	move_and_slide()
 	
-func slash(delta):
-	if not slashing:
-		axis = get_global_mouse_position() - self.global_position
-		axis = axis.normalized()
-		velocity += axis * target_speed * DASH_MULTIPLIER
-		slashing = true
-	else:
-		if velocity.length() > (DASH_FRICTION * delta):
-			velocity -= velocity.normalized() * (DASH_FRICTION * delta)
-		else:
-			velocity = Vector2.ZERO
-			slashing = false
-		
-	move_and_slide()
-	
+
+
+
 func _on_stairs_right_body_entered(body):
 	stair_type = StairTypes.RIGHT
 func _on_stairs_left_body_entered(body):
@@ -106,7 +88,3 @@ func _on_stairs_left_body_exited(body):
 	stair_type = StairTypes.NONE
 func _on_stairs_up_down_body_exited(body):
 	stair_type = StairTypes.NONE
-
-
-func _on_mc_death_body_entered(body):
-	print("ded")
